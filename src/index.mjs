e5f9830f54372ef88d56f7202f63bb89ce2244c0
jmoj95 <jmoj95@protonmail.com>
2024-08-02 20:30:05 -0300

    Express JS #2 - Get Requests

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send({
+        message: "Hello, world!!!"
+    });
+});
+
+app.get('/api/users', (req, res) => {
+    res.send([
+        { id: 1, username: 'Anson', displayName: 'Anson' },
+        { id: 2, username: 'Jack', displayName: 'Jack' },
+        { id: 3, username: 'Adam', displayName: 'Adam' }
+    ]);
+});
+
+app.get('/api/products', (req, res) => {
+    res.send([
+        { id: 123, name: 'Chicken Breast', price: 12.99 }
+    ]);
+});
+
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
