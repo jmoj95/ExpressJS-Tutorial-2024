77755aa09ecf1854de37f9882938dae5010ee8d8
jmoj95 <jmoj95@protonmail.com>
2024-08-03 04:05:12 -0300

    Express JS #3.5 - Code Refactoring

--- src/index.mjs
+++ src/index.mjs
@@ -10,9 +10,27 @@ const mockUsers = [
     { id: 3, username: 'Adam', displayName: 'Adam' }
 ];
 
+const mockProducts = [
+    { id: 123, name: 'Chicken Breast', price: 12.99 }
+];
+
+const isIdValid = (id) => {
+    id = id.toString();
+    const n1 = Math.abs(id);
+    const n2 = parseInt(id, 10);
+    return !isNaN(n1) && n2 === n1 && n1.toString() === id;
+};
+
+const getErrorResponse = (message) => {
+    return {
+        error: true,
+        message: message
+    };
+};
+
 app.get('/', (req, res) => {
     res.send({
-        message: "Hello, world!!!"
+        message: 'Hello, world!!!'
     });
 });
 
@@ -22,28 +40,20 @@ app.get('/api/users', (req, res) => {
 
 app.get('/api/users/:id', (req, res) => {
     const parsedId = parseInt(req.params.id);
-    if (isNaN(parsedId)) {
-        return res.status(400).send({
-            error: true,
-            message: "Bad Request: Invalid ID."
-        });
+    if (!isIdValid(parsedId)) {
+        return res.status(400).send(getErrorResponse('Bad Request: Invalid ID.'));
     }
 
     const user = mockUsers.find(user => user.id === parsedId);
     if (!user) {
-        return res.status(404).send({
-            error: true,
-            message: "User not found"
-        });
+        return res.status(404).send(getErrorResponse('User not found'));
     }
 
     return res.send(user);
 });
 
 app.get('/api/products', (req, res) => {
-    res.send([
-        { id: 123, name: 'Chicken Breast', price: 12.99 }
-    ]);
+    res.send(mockProducts);
 });
 
 app.listen(PORT, () => {
