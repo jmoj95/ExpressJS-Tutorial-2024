b0837a05a20d6c087124319294e5fa2d50bf6484
jmoj95 <38600458+jmoj95@users.noreply.github.com>
2024-08-03 14:11:18 -0300

    Implement environment variables to the project
    - Added environment variable file as parameter in `npm start` and `npm start:dev`
    - Added `.env.template` to indicate the expected environment variables
    - Added `.env` files to `.gitignore`
    - Removed fallback port to PORT variable in `index.mjs`

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const mockUsers = [
     { id: 1, username: 'Anson', displayName: 'Anson' },
