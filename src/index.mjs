4c962973fd1cf159168e9e16273e1ba6d89f9379
jmoj95 <38600458+jmoj95@users.noreply.github.com>
2024-08-05 18:03:50 -0300

    Express JS #12 - Cookies
    - Implemented code from the `Express JS #12 - Cookies` course

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,26 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import loggingMiddleware from './middlewares/loggingMiddleware.mjs';
 import routes from './routes/index.mjs';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser('helloworld'));
 app.use('/api', routes);
 
 const PORT = process.env.PORT;
 
 app.get('/', loggingMiddleware, (req, res) => {
-    res.send({
+    const helloCookie = req.signedCookies['hello'];
+    if (!helloCookie || helloCookie !== 'world' ) {
+        res.cookie('hello', 'world', {
+            maxAge: 2 * 60 * 60 * 1000,
+            signed: true
+        });
+    }
+
+    return res.send({
         message: 'Hello, world!!!'
     });
 });
--- src/routes/product.mjs
+++ src/routes/product.mjs
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
 import * as productsHelper from '../helpers/productHelper.mjs';
+import * as responseHelper from '../helpers/responseHelper.mjs';
 
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send(productsHelper.getAllProducts());
+    console.log(req.headers.cookie);
+    console.log(req.cookies);
+    console.log(req.signedCookies['hello']);
+
+    const helloCookie = req.signedCookies['hello'];
+    if (!helloCookie || helloCookie !== 'world' ) {
+        return res.status(403).send(responseHelper.getErrorResponse('Incorrect or expired cookie'));
+    }
+
+    return res.send(productsHelper.getAllProducts());
 });
 
 export default router;
