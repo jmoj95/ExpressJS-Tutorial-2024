0a8dd03fcc37a4d15c33b4288eb96e21090f602d
jmoj95 <38600458+jmoj95@users.noreply.github.com>
2024-08-11 03:47:38 -0300

    Express JS #13 - Sessions Pt. 1 (#26)

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,33 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
 import loggingMiddleware from './middlewares/loggingMiddleware.mjs';
 import routes from './routes/index.mjs';
+
+const PORT = process.env.PORT;
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser('helloworld'));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(session({
+    secret: SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    }
+}));
 app.use('/api', routes);
 
-const PORT = process.env.PORT;
-
 app.get('/', loggingMiddleware, (req, res) => {
+    console.log(req.session);
+    console.log(req.session.id);
+
+    req.session.visited = true;
+
     const helloCookie = req.signedCookies['hello'];
     if (!helloCookie || helloCookie !== 'world' ) {
         res.cookie('hello', 'world', {
